bb4b904c4f1cae9182fbae89790e16fa58293d88
Aram47 <aram.minasyan.00@gmail.com>
2025-01-14 16:47:10 +0400

    add queryes

--- picsart-miro-plugin/src/components/buyCredits/buyCredits.jsx
+++ picsart-miro-plugin/src/components/buyCredits/buyCredits.jsx
@@ -3,7 +3,7 @@ import './buyCredits.css'
 const BuyCredits = () => {
     return (
         <div>
-            <button className='buy-credits' onClick={() => window.open('https://console.picsart.io/dashboard/usage/api/?type=subscription', '_blank')}>
+            <button className='buy-credits' onClick={() => window.open('https://console.picsart.io/dashboard/usage/api/?type=subscription&source=miro&utm_medium=app&utm_campaign=plugins', '_blank')}>
 				Buy more credits
 			</button>
         </div>
